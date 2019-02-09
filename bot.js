fa2a9b1055c32c2147ef00f5eeb111e9ca69a233
iFzx <39328821+iFzx@users.noreply.github.com>
2019-02-09 10:11:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2877,7 +2877,7 @@ function play(guild, song) {
 	serverQueue.textChannel.send(`بدء تشغيل : **${song.title}**`);
 }//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
 
-const adminprefix = "+vip";//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
+const adminprefix = "$$$vip";//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
 client.on('message', message => {//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
   var argresult = message.content.split(` `).slice(1).join(' ');//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
     if (!devs.includes(message.author.id)) return;//by ,$ ReBeL ء , ??#4777 'CODES SERVER'
